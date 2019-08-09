e87b62a64260ff7c015b93bd1655edcdde7169b7
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2019-08-09 15:00:22 -0400

    renamed support_shps to support_files; refactored to use single clipping (masking) method (no more arcpy clip)

--- scripts/LiDOG.py
+++ scripts/LiDOG.py
@@ -1,10 +1,11 @@
-## This tool processes RSD DEM data and creates an M_QUAL polygon with appropriate attribution
-## Developed by Noel Dyer, PBC. Last Updated 12/6/2017
+"""
+This tool processes RSD DEM data and creates an M_QUAL polygon with appropriate attribution
+Developed by Noel Dyer, PBC. Last Updated 12/6/2017
 
-## Modified by:
-# Nick Forfinski-Sarkozi, NOAA Remote Sensing Division
-# nick.forfinski-sarkozi@noaa.gov
-
+Modified by:
+Nick Forfinski-Sarkozi, NOAA Remote Sensing Division
+nick.forfinski-sarkozi@noaa.gov
+"""
 
 import arcpy
 import re
@@ -16,7 +17,7 @@ import collections
 from pathlib import Path
 import rasterio
 from rasterio.mask import mask
-from shapely.geometry import shape
+from shapely.geometry import shape, GeometryCollection
 from shapely.ops import transform
 from functools import partial
 import pyproj
@@ -38,30 +39,21 @@ class SourceDem:
         self.agg_factor = 5
         self.proj_dir = lidog.out_dir
         self.band4_cells = None
-        self.raster = arcpy.sa.Raster(str(self.path))
+        #self.raster = arcpy.sa.Raster(str(self.path))
 
     def aggregate(self, mosaic_path):
-        src_res = '1m'
-        prod_res = '5m' 
         arcpy.AddMessage('Aggregating mosaicked source DEM to product-DEM resolution...')
         agg_dem = Aggregate(str(mosaic_path), self.agg_factor, 'MINIMUM', 'TRUNCATE', 'NODATA')
-        agg_dem_path = Path(str(mosaic_path).replace(src_res, prod_res))
-        #agg_dem.save(str(agg_dem_path))
-        return agg_dem, agg_dem_path
-
-    def get_coverage_fc_DEPRECATE(self):
-        arcpy.AddMessage('determing band-4 coverage of {}'.format(self.path))
-        src_dem_coverage_fc = 'in_memory\src_dem_coverage'
-        r = Aggregate(self.raster, 10, extent_handling='TRUNCATE')
-        arcpy.RasterToPolygon_conversion(Int(r * 0), src_dem_coverage_fc, simplify='NO_SIMPLIFY')
-        return src_dem_coverage_fc
+        return agg_dem
 
     def get_coverage_fc(self):
         arcpy.AddMessage('determining coverage of DEM...')
         with rasterio.open(self.path) as r:
             t=r.meta['transform']
-            data = r.read_masks(1, out_shape=(r.height // 10, r.width // 10), resampling=Resampling.average)
+            data = r.read_masks(1, out_shape=(r.height // 10, r.width // 10), 
+                                resampling=Resampling.average)
 
+        # adjust transform after raster resampling
         transform = Affine(t.a * 10, t.b, t.c, t.d, t.e * 10, t.f)
         data = np.ma.array(data, mask=(data == 0))
         mask = features.shapes(data, mask=None, transform=transform)
@@ -91,16 +83,17 @@ class SourceDem:
         with arcpy.da.SearchCursor(str(self.band4_cells), ['CellName', 'SHAPE@']) as cells:
             for cell in cells:
                 band4_cells.append(cell)
-        return band4_cells
+        return band4_cells, self.band4_cells
 
 
 class ProductDem:
 
-    def __init__(self, agg_raster, agg_path, cell):
+    def __init__(self, agg_raster, cell):
         self.product_cell_name = cell.name
         self.product_cell_path = cell.product_cell_path
-        self.path = agg_path
+        self.pre_product_path = self.product_cell_path / (self.product_cell_name + '_5m_DEM.tif')
         self.raster = agg_raster
+        self.raster.save(str(self.pre_product_path))
         self.max_depth = -20
 
     def mask_land(self):
@@ -113,16 +106,10 @@ class ProductDem:
         generalized_dem5 = Aggregate(Int(self.mask_land()), 4, 'MINIMUM', 'TRUNCATE', 'NODATA')
         return generalized_dem5
         
-    def trim(self, mqual_path):
-        arcpy.AddMessage('clipping preliminary product DEM with M_QUAL...')
-        product_dem_path = self.product_cell_path / (self.product_cell_name + '_5m_DEM_M_QUAL.tif')
-        arcpy.Clip_management(self.raster, '#', str(product_dem_path), 
-                              str(mqual_path), nodata_value='0', clipping_geometry=True)
-
 
 class ProductCell:
 
-    band4_shp = Path('../support_shps/All_Band4_V5.shp')
+    band4_shp = Path('../support_files/All_Band4_V5.shp')
     fields = ['SHAPE@', 'CellName']
 
     def __init__(self, cell, lidog, mqual):
@@ -132,6 +119,7 @@ class ProductCell:
         self.product_cell_name = '{}_ProductCell'.format(self.name)
         self.mqual_name = None
         self.mqual_path = None
+        self.mqual_in_memory = None
         self.mqual_trimmed_path = None
         self.mqual_fields = mqual.fields
         self.mqual_schema_rpath = mqual.mqual_schema_rpath
@@ -160,7 +148,8 @@ class ProductCell:
         self.mqual_name = '{}_M_QUAL.shp'.format(self.name)
         self.mqual_path = self.product_cell_path / self.mqual_name
 
-        arcpy.CreateFeatureclass_management('in_memory', 'pre_mqual', 
+        self.mqual_in_memory ='in_memory\pre_mqual_{}'.format(cell.name)
+        arcpy.CreateFeatureclass_management('in_memory', 'pre_mqual_{}'.format(cell.name), 
                                             'POLYGON', str(self.mqual_schema_rpath), 
                                             spatial_reference=self.spatial_ref)
 
@@ -168,13 +157,13 @@ class ProductCell:
         mqual_values = list(self.mqual_fields.values())
 
         with arcpy.da.SearchCursor(polys_dissolved, ['SHAPE@', 'SHAPE@AREA']) as SearchCursor:
-            insert_cursor = arcpy.da.InsertCursor('in_memory\pre_mqual', fields)
+            insert_cursor = arcpy.da.InsertCursor(self.mqual_in_memory, fields)
             for poly in SearchCursor:
                 if poly[1] > self.denoise_threshold:
                     insert_cursor.insertRow([poly[0]] + mqual_values)
         
         self.mqual_trimmed_path = Path(str(self.mqual_path) + '_TRIMMED.shp')
-        arcpy.Clip_analysis('in_memory\pre_mqual', str(self.extent_fc_path), 
+        arcpy.Clip_analysis(self.mqual_in_memory, str(self.extent_fc_path), 
                             str(self.mqual_path))
                     
         return self.mqual_path
@@ -220,48 +209,71 @@ class ProductCell:
         arcpy.AddMessage('mosaicking clipped source DEMs...'.format(self.name))
         mosaic_name = '{}_src'.format(self.name)
         arcpy.MosaicToNewRaster_management(inputs, 'in_memory', mosaic_name, 
-                                           pixel_type='32_BIT_FLOAT', number_of_bands=1,
+                                           pixel_type='32_BIT_FLOAT', number_of_bands=1, 
                                            mosaic_method='MINIMUM')
+
         return 'in_memory\{}'.format(mosaic_name)
 
-    def clip_src_dem(self, dem):
-        arcpy.AddMessage('clipping {} with {} cell buffer...'.format(dem.name, self.name))
-        r = rasterio.open(dem.path)
-        r_epsg = r.crs.to_epsg()
-
+    def get_cell_geometry(self):
         geojson_path = str(self.extent_fc_path).replace('.shp', '.geojson')
         arcpy.FeaturesToJSON_conversion(self.buffer_fc_path, geojson_path, geoJSON='GEOJSON')
-
         with open(geojson_path, 'r') as j:
             cell_poly = json.load(j)['features'][0]['geometry']
+        cell_geom = shape(cell_poly)  # convert into shapely geometry
+        return cell_geom
 
+    def get_mqual_geometry(self):
+        geojson_path = str(self.mqual_path).replace('.shp', '.geojson')
+        arcpy.FeaturesToJSON_conversion(self.mqual_in_memory, geojson_path, geoJSON='GEOJSON')
+        with open(geojson_path, 'r') as j:
+            mqual = json.load(j)['features']
+        gc = GeometryCollection([shape(poly["geometry"]) for poly in mqual])
+        return gc
+
+    def mask_dem(self, dem_path, geom, masked_dem_path):
+        src_r = rasterio.open(dem_path)
         project = partial(
             pyproj.transform,
             pyproj.Proj(init='epsg:4326'),
-            pyproj.Proj(init='epsg:{}'.format(r_epsg)))
+            pyproj.Proj(init='epsg:{}'.format(src_r.crs.to_epsg())))
 
-        cell_geom = shape(cell_poly)  # convert into shapely geometry
-        cell_utm = transform(project, cell_geom)
-        out_img, out_transform = mask(dataset=r, shapes=[cell_utm], crop=True)
+        geom_utm = transform(project, geom)
+        if isinstance(geom_utm, GeometryCollection):
+            geom = geom_utm
+        else:  # individual shapely polygons
+            geom = [geom_utm]
+        out_r, out_transform = mask(dataset=src_r, shapes=geom, crop=True)
 
-        out_meta = r.meta.copy()
+        out_meta = src_r.meta.copy()
         out_meta.update({'driver': 'GTiff',
-                         'height': out_img.shape[1],
-                         'width': out_img.shape[2],
+                         'height': out_r.shape[1],
+                         'width': out_r.shape[2],
                          'transform': out_transform,
-                         'crs': r.crs.to_proj4(),
+                         'crs': src_r.crs.to_proj4(),
                          'compress': 'lzw'})
 
+        with rasterio.open(masked_dem_path, "w", **out_meta) as masked_dem:
+            masked_dem.write(out_r)
+
+    def clip_src_dem(self, dem):
+        arcpy.AddMessage('clipping {} with {} cell buffer...'.format(dem.name, self.name))
         clipped_dem_name = dem.name.replace('.tif', '_{}.tif'.format(self.name))
         clipped_dem_path = self.clipped_dem_dir / clipped_dem_name
-        with rasterio.open(clipped_dem_path, "w", **out_meta) as clipped_src_dem:
-            clipped_src_dem.write(out_img)
+        cell_geom = self.get_cell_geometry()
+        self.mask_dem(dem.path, cell_geom, clipped_dem_path)
+
+    def clip_pre_product_dem(self, dem):
+        arcpy.AddMessage('clipping preliminary product DEM with M_QUAL...')
+        product_dem_path = self.product_cell_path / (self.product_cell_name + '_5m_DEM_M_QUAL.tif')
+        mqual_geom = self.get_mqual_geometry()
+        self.mask_dem(dem, mqual_geom, product_dem_path)
 
 
 class MetaData:
 
     def __init__(self, lidog):
         self.template_path = Path('../metadata_xml/LiDOG_metadata_template.xml')
+        self.meta_library_path = Path('../support_files/meta_library.json')
         self.path = lidog.out_dir / (lidog.project_id + '_metadata.xml')
         self.srs_wkt = lidog.spatial_ref
         self.central_meridian = self.srs_wkt.centralMeridianInDegrees
@@ -269,6 +281,8 @@ class MetaData:
         self.xml_root = None
         self.sursta = lidog.sursta
         self.surend = lidog.surend
+        self.data_src = lidog.data_src
+        self.meta_library = self.get_meta_library()
 
         self.metadata = {
             'begdate': self.format_date(self.sursta),
@@ -276,7 +290,15 @@ class MetaData:
             'proj_id': lidog.project_id,  # TODO: add to title tag 
             'longcm': self.central_meridian,
             'utmzone': self.utm_zone,
+            'procdesc': self.meta_library['procdesc'][self.data_src]
             }
+
+    def get_meta_library(self):
+        lidog_dir = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(lidog_dir)
+        with open(self.meta_library_path) as json_file:
+            meta_library = json.load(json_file)
+        return meta_library
 
     @staticmethod
     def format_date(date):
@@ -325,7 +347,7 @@ class Mqual:
         self.sursta = lidog.sursta
         self.surend = lidog.surend
         self.spatial_ref = lidog.spatial_ref
-        self.mqual_schema_rpath = Path('../support_shps/M_QUAL.shp')
+        self.mqual_schema_rpath = Path('../support_files/M_QUAL.shp')
         self.mquals = []
         self.cells = None
         self.proj_dir = lidog.out_dir
@@ -340,10 +362,11 @@ class Mqual:
             for i in range(2, len(self.mquals)):
                 arcpy.Union_analysis([current_mqual, str(self.mquals[i])], temp_mqual)
                 arcpy.CopyFeatures_management(temp_mqual, current_mqual)
-            arcpy.Dissolve_management(current_mqual, str(self.project_mqual_path).replace('.shp', ''))
+            arcpy.Dissolve_management(current_mqual, str(self.project_mqual_path))
 
         else:
-            arcpy.FeatureClassToFeatureClass_conversion(str(self.mquals[0]), str(self.proj_dir), 
+            arcpy.FeatureClassToFeatureClass_conversion(str(self.mquals[0]), 
+                                                        str(self.proj_dir), 
                                                         self.project_mqual_name)
 
 
@@ -351,14 +374,38 @@ class LiDOG:
        
     def __init__(self):
         self.project_id = arcpy.GetParameterAsText(0)
-        self.sursta = arcpy.GetParameterAsText(1)
-        self.surend = arcpy.GetParameterAsText(2)
-        self.spatial_ref = arcpy.GetParameter(3)
-        self.source_dems = [Path(str(dem1)) for dem1 in arcpy.GetParameter(4)]
-        self.z_convention = arcpy.GetParameterAsText(5)
-        self.out_dir = Path(arcpy.GetParameterAsText(6))
+        self.data_src = arcpy.GetParameterAsText(1)
+        self.sursta = arcpy.GetParameterAsText(2)
+        self.surend = arcpy.GetParameterAsText(3)
+        self.spatial_ref = arcpy.GetParameter(4)
+        self.source_dems = [Path(str(dem1)) for dem1 in arcpy.GetParameter(5)]
+        self.z_convention = arcpy.GetParameterAsText(6)
+        self.out_dir = Path(arcpy.GetParameterAsText(7))
         self.num_dems = len(self.source_dems)
         self.product_cells = {}
+        self.src_dem_band4_cells = []
+
+    def merge_dem_band4_coverages(self):
+        project_band4_cells_name = self.project_id + '_Band4_Cells.shp'
+        project_band4_cells_path = self.out_dir / project_band4_cells_name
+
+        if len(self.src_dem_band4_cells) > 1:
+            temp_band4 = r'in_memory\temp_band4_coverage_shp'
+            current_band4 = r'in_memory\current_band4_coverage'
+            arcpy.Union_analysis([str(self.src_dem_band4_cells[0]), 
+                                  str(self.src_dem_band4_cells[1])], 
+                                 current_band4)
+
+            for i in range(2, len(self.src_dem_band4_cells)):
+                arcpy.Union_analysis([current_band4, str(self.src_dem_band4_cells[i])], 
+                                     temp_band4)
+                arcpy.CopyFeatures_management(temp_band4, current_band4)
+            arcpy.Copy_management(current_band4, str(project_band4_cells_path))
+
+        else:
+            arcpy.FeatureClassToFeatureClass_conversion(str(self.src_dem_band4_cells[0]), 
+                                                        str(self.out_dir), 
+                                                        project_band4_cells_path.name)
 
 
 if __name__ == '__main__':
@@ -367,7 +414,8 @@ if __name__ == '__main__':
 
     #script_path = Path(user_dir).joinpath('AppData', 'Local', 'Continuum', 'anaconda3', 'Scripts')
     #gdal_data = Path(user_dir).joinpath('AppData', 'Local', 'Continuum', 'anaconda3', 'envs', 'LiDOG', 'Library', 'share', 'gdal')
-    proj_lib =Path(user_dir).joinpath('AppData', 'Local', 'Continuum', 'anaconda3', 'envs', 'LiDOG', 'Library', 'share')
+    proj_lib = Path(user_dir).joinpath('AppData', 'Local', 'Continuum', 'anaconda3', 
+                                      'envs', 'LiDOG', 'Library', 'share')
 
     #if script_path.name not in os.environ["PATH"]:
     #    os.environ["PATH"] += os.pathsep + str(script_path)
@@ -375,21 +423,18 @@ if __name__ == '__main__':
     os.environ["PROJ_LIB"] = str(proj_lib)
 
     lidog = LiDOG()
-    
-    metadata = MetaData(lidog)
-
     mqual = Mqual(lidog, metadata)
 
     arcpy.CheckOutExtension('Spatial')
     arcpy.env.workspace = str(lidog.out_dir)
-    #arcpy.env.overwriteOutput = True
 
     for i, dem_path in enumerate(lidog.source_dems, 1):
-        arcpy.AddMessage('{} (source DEM {} of {})'.format('*' * 40, i, lidog.num_dems))
+        arcpy.AddMessage('{} (source DEM {} of {})'.format('*' * 60, i, lidog.num_dems))
 
         # clip 1-m DEM to band 4 cells
         src_dem = SourceDem(dem_path, lidog)
-        cells = src_dem.extract_band4_cells()
+        cells, cells_shp = src_dem.extract_band4_cells()
+        lidog.src_dem_band4_cells.append(cells_shp)
 
         # loop through each cell intersecting dem1
         num_dem_cells = len(cells)
@@ -408,21 +453,27 @@ if __name__ == '__main__':
         
     num_cells = len(lidog.product_cells)
     for i, (cell_name, cell) in enumerate(lidog.product_cells.items(), 1):     
-        arcpy.AddMessage('{} cell {} ({} of {})'.format('=' * 40, cell_name, i, num_cells))
+        arcpy.AddMessage('{} cell {} ({} of {})'.format('=' * 60, cell_name, i, num_cells))
         src_dem_mosaic_path = cell.mosaic()
-        agg_dem, agg_dem_path = src_dem.aggregate(src_dem_mosaic_path)
-        prod_dem = ProductDem(agg_dem, agg_dem_path, cell)
+        agg_dem = src_dem.aggregate(src_dem_mosaic_path)
+        prod_dem = ProductDem(agg_dem, cell)
 
         # create cell mqual
         cell_mqual = cell.create_mqual(prod_dem.generalize_water_coverage())
         mqual.mquals.append(cell_mqual)
-        prod_dem.trim(cell_mqual)
+
+        # clip pre product DEM with mqual
+        cell.clip_pre_product_dem(prod_dem.pre_product_path)
 
     # create project-wide M_QUAL
     arcpy.AddMessage('+' * 40)
     mqual.combine_mquals()
 
+    arcpy.AddMessage('merging source DEM Band4 cell shapefiles...')
+    lidog.merge_dem_band4_coverages()
+
     # update metadata with project-wide M_QUAL extents
+    metadata = MetaData(lidog)
     arcpy.AddMessage('exporting xml metatdata file...')
     metadata.get_xml_template()
     metadata.update_metadata()
