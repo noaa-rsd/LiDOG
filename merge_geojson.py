4ca8bc4df0dd5f31ecbed50496cdacd5203fdb41
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2019-08-23 14:35:06 -0400

    swapped out environment file

--- merge_geojson.py
+++ merge_geojson.py
@@ -6,6 +6,7 @@ import shapely.geometry
 from shapely.geometry import shape
 import shapely.ops
 import arcpy
+import collections
 
 
 shp_dir = r'X:\iocm_deliverables\ocs\GL07-16-TB-J\michigan\PRODUCT_CELLS'
@@ -14,6 +15,7 @@ p_dir = Path(shp_dir)
 geojsons = p_dir.rglob('*_mqual.geojson')
 
 mqual_polys = None
+sr = arcpy.SpatialReference(26916)
 
 for i, g in enumerate(list(geojsons)):
     with open(g) as f:
@@ -22,25 +24,41 @@ for i, g in enumerate(list(geojsons)):
 
             with open(g, 'r') as j:
                 mqual = json.load(j)['features']
-                print(json.dumps(mqual[0], indent=1))
+
             gc = [shape(poly["geometry"]).buffer(0) for poly in mqual]
 
             if i == 0:
                 mqual_polys = gc
+                properties = mqual[0]['properties']
             else:
                 mqual_polys = mqual_polys + gc
         except Exception as e:
             print(e)
 
+print('merging {} shapely polygons...'.format(len(mqual_polys)))
+merged_mqual = shapely.ops.cascaded_union(mqual_polys)
 
-#print('merging {} shapely polygons...'.format(len(mqual_polys)))
-#merged_mqual = shapely.ops.cascaded_union(mqual_polys)
-#print(merged_mqual)
-#arc_polys = arcpy.FromWKT(merged_mqual.to_wkt(), arcpy.SpatialReference(26916))
-#print(arc_polys)
+print('copying features to shapefile...')
+mqual_shp_dir = r'X:\iocm_deliverables\ocs\GL07-16-TB-J\michigan\PRODUCT_CELLS'
+mqual_shp = r'X:\iocm_deliverables\ocs\GL07-16-TB-J\michigan\PRODUCT_CELLS\mqual11.shp'
 
-#mqual_shp = r'X:\iocm_deliverables\ocs\GL07-16-TB-J\michigan\PRODUCT_CELLS\mqual.shp'
-#arcpy.CopyFeatures_management(arc_polys, mqual_shp)
+fields = collections.OrderedDict([
+    ('CATZOC', 3), 
+    ('POSACC', 1), 
+    ('SOUACC', 0.5), 
+    ('SUREND', '20090327'),
+    ('SURSTA', '20090327'), 
+    ('TECSOU', 7), 
+    ('INFORM', 'NOAA NGS-RSD'), 
+    ('SORDAT', '20090327'), 
+    ('SORIND', 'US,US,graph'), 
+    ('FCSubtype', 40)
+    ])
 
+arcpy.CreateFeatureclass_management(mqual_shp_dir, 'mqual11.shp', 'POLYGON',
+                                    str(g).replace('.geojson', '.shp'), 
+                                    spatial_reference=sr)
 
-
+mquals = arcpy.da.InsertCursor(mqual_shp, ['SHAPE@WKT'] + list(fields.keys()))
+mquals.insertRow([merged_mqual.to_wkt()] + list(fields.values()))
+del mquals
