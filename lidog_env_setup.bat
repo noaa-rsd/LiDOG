2ae5fcf8c0c16490a2da5377a971b4c249323770
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2020-05-15 14:32:13 -0400

    added prompt for conda env dir in bat

--- lidog_env_setup.bat
+++ lidog_env_setup.bat
@@ -1,3 +1,4 @@
-call %UserProfile%\AppData\Local\Continuum\anaconda3\condabin\conda.bat create --clone "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3" --prefix %UserProfile%\AppData\Local\Continuum\anaconda3\envs\lidog
+set /P env_dir="Enter conda environments directory: "
+call %UserProfile%\AppData\Local\Continuum\anaconda3\condabin\conda.bat create --clone "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3" --prefix %env_dir%\lidog
 call %UserProfile%\AppData\Local\Continuum\anaconda3\condabin\conda.bat activate lidog
 call %UserProfile%\AppData\Local\Continuum\anaconda3\condabin\conda.bat install -c conda-forge rasterio shapely pyproj
